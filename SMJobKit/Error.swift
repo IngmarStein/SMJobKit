b77fc2c1aa3b27196bdbc8454519f752ddb1b22c
IngmarStein <IngmarStein@users.noreply.github.com>
2015-04-08 15:09:17 +0200

    Convert NSError subclass into an extension

--- SMJobKit/Error.swift
+++ SMJobKit/Error.swift
@@ -6,24 +6,18 @@
 //
 //
 
-import Cocoa
+import Foundation
 
-class Error : NSError {
-	init(code:ErrorCode, message:String) {
-		let userInfo = [ NSLocalizedDescriptionKey:message ]
-
-		super.init(domain:"SMJobKit", code:code.rawValue, userInfo:userInfo)
-	}
-
-	required init(coder aDecoder: NSCoder) {
-	    fatalError("init(coder:) has not been implemented")
+extension NSError {
+	convenience init(code:ErrorCode, message:String) {
+		self.init(domain:"SMJobKit", code:code.rawValue, userInfo:[ NSLocalizedDescriptionKey:message ])
 	}
 }
 
 func SET_ERROR(error: NSErrorPointer, code: ErrorCode, messageFormat: String, arguments: CVarArgType...) {
 	if error != nil {
 		let message = String(format: messageFormat, arguments: arguments)
-		NSLog("[SMJKit Error] %@", message)
-		error.memory = Error(code:code, message:message)
+		NSLog("[SMJobKit Error] %@", message)
+		error.memory = NSError(code:code, message:message)
 	}
 }
