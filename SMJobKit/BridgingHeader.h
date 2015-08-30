e56b9e3985965589d059429eb2cb998eeca32e15
IngmarStein <IngmarStein@users.noreply.github.com>
2015-08-30 16:01:00 +0200

    Workaround for https://forums.developer.apple.com/message/24971

---
+++ SMJobKit/BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgingHeader.h
+//  SMJobKit
+//
+//  Created by Ingmar Stein on 30.08.15.
+//
+//
+
+#import <ServiceManagement/ServiceManagement.h>
--- SMJobKit/Client.swift
+++ SMJobKit/Client.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import ServiceManagement
 
 public class Client {
 
